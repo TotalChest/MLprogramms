9584fc6eb4dd60b8a9ce7429d8ad81fbba39dc86
TotalChest <13sergey78@mail.ru>
2020-02-03 19:21:37 +0300

    Fix backpropagation

--- Neural_Network/Data_Generation.py
+++ Neural_Network/Data_Generation.py
@@ -1,20 +1,34 @@
+'''
+Скрипт генерации векторов изображений
+
+Изображения хранятся в папках с названиями из folders. Названия изображений "i.bmp", где i - номера изображений по порядку.
+
+Вектора  записываются в один файл в следующем формате:
+Class_name:Class_number:0,0,1,0,1,1,0,1,0,0,0,1, ... ,1,0,0,0,0,0,1,0
+'''
+
+
 from PIL import Image
 import os
 
 
+dst_file = "data_set.txt"	# Файл векторов
+size = 20	# Размер стороны квадатного изображения
+
+
 folders = ['Circle', 'Triangle', 'Cross']
 dictionary = dict(zip(folders, range(len(folders))))
-handle = open("data_set.txt", "w")
+handle = open(dst_file, "w")
 
 
 for dir in folders:
-    for image_num in range(len(os.listdir(dir))):
-        handle.write(dir + ':' + str(dictionary[dir]) + ':')
-        image = Image.open(dir + '/' + str(image_num + 1) + '.bmp')
-        pix = image.load()
-        for i in range(20):
-            for j in range(20):
-                handle.write(('1' if int((pix[i, j][0] + pix[i, j][1] + pix[i, j][2]) / 3) < 128 else '0') + (
-                    ',' if (i + j != 38) else ''))
-        handle.write('\n')
+	for image_num in range(len(os.listdir(dir))):
+		handle.write(dir + ':' + str(dictionary[dir]) + ':')
+		image = Image.open(dir + '/' + str(image_num + 1) + '.bmp')
+		image.thumbnail((size, size))
+		pix = image.load()
+		for i in range(size):
+			for j in range(size):
+				handle.write(('1' if int((pix[i, j][0] + pix[i, j][1] + pix[i, j][2]) / 3) < 128 else '0') + (',' if (i + j != 2 * (size - 1)) else ''))
+		handle.write('\n')
 handle.close()
--- Neural_Network/NeuroNet.py
+++ Neural_Network/NeuroNet.py
@@ -2,41 +2,49 @@ import numpy as np
 from scipy import optimize
 import functions
 
-dictionary = {0: 'Circle', 1: 'Triangle', 2: 'Cross'}
 
+src_file = 'data_set.txt'	# Файл с подготовленными векторами
+input_layer_size = 400	# Длина входного вектора изображения
+hidden_layer_1_size = 32	# Количество нейронов первого скрытого слоя
+hidden_layer_2_size = 16	# Количество нейронов второго скрытого слоя
+num_labels = 3	# Количество меток
+options = {'maxiter': 100}	# Настройки оптимизатора
+lambda_ = 0	# Параметр регуляризации
+
+
+dictionary = {0: 'Circle', 1: 'Triangle', 2: 'Cross'}
 Y = []
 X = []
-file = open('data_set.txt', 'r')
+file = open(src_file, 'r')
 data = file.readlines()
 for i in data:
-    Y.append(i.split(':')[1])
+    Y.append(int(i.split(':')[1]))
     X.append(list(map(int, i.split(':')[2].split(','))))
+X = np.array(X)	# Векторы по строкам (m, 400)
+Y = np.array(Y)	# Метки векторов (m)
+m = Y.size	# Размер обучающей выборки
+layers = (input_layer_size, hidden_layer_1_size, hidden_layer_2_size, num_labels)	# Корреж архитектуры неросети
 
-X = np.array(X)  # labels
-Y = np.array(Y)  # data vectors
-m = Y.size  # size of learning set
 
-input_layer_size = 400
-hidden_layer_1_size = 32
-hidden_layer_2_size = 16
-output_layer_size = 3
+initial_Theta1 = functions.randInitialize(hidden_layer_1_size, input_layer_size + 1)	# Theta1 (32, 401)
+initial_Theta2 = functions.randInitialize(hidden_layer_2_size, hidden_layer_1_size + 1)	# Theta2 (16, 33)
+initial_Theta3 = functions.randInitialize(num_labels, hidden_layer_2_size + 1)	# Theta3 (3, 17)
 
-weghts_to_hidden_1 = np.random.random((32, 401))
-weights_to_hidden_2 = np.random.random((16, 33))
-weights_to_output = np.random.random((3, 17))
+initial_nn_params = np.concatenate([initial_Theta1.ravel(), initial_Theta2.ravel()], axis=0)
+initial_nn_params = np.concatenate([initial_nn_params, initial_Theta3.ravel()], axis=0)
 
-initial_nn_params = np.concatenate([weghts_to_hidden_1.ravel(), weights_to_hidden_2.ravel()], axis=0)
-initial_nn_params = np.concatenate([initial_nn_params, weights_to_output.ravel()], axis=0)
-
-options = {'maxiter': 500}
-
-lambda_ = 0
-
-costFunction = lambda p: functions.nnCostFunction(p, input_layer_size,
+'''
+J,_=functions.nnCostFunction(initial_nn_params, (input_layer_size,
                                                   hidden_layer_1_size,
                                                   hidden_layer_2_size,
-                                                  output_layer_size, X, Y, lambda_)
+                                                  num_labels), X, Y, 0)
 
+print(J,_)
+'''
+costFunction = lambda p: functions.nnCostFunction(initial_nn_params, layers, X, Y, lambda_)
+nn_params, J_history = functions.gradientDescent(costFunction, initial_nn_params, 0.1, 100)
+print(J_history)
+'''
 res = optimize.minimize(costFunction,
                         initial_nn_params,
                         jac=True,
@@ -44,18 +52,15 @@ res = optimize.minimize(costFunction,
                         options=options)
 
 nn_params = res.x
+print(costFunction(nn_params))
+'''
 
-weights_h_1 = np.reshape(nn_params[:hidden_layer_1_size * (input_layer_size + 1)],
-                         (hidden_layer_1_size, (input_layer_size + 1)))
-
-weights_h_2 = np.reshape(nn_params[(hidden_layer_1_size * (input_layer_size + 1)):(hidden_layer_1_size * (
-        input_layer_size + 1)) + hidden_layer_2_size * (hidden_layer_1_size + 1)],
-                         (hidden_layer_2_size, (hidden_layer_1_size + 1)))
-
-weights_o = np.reshape(
-    nn_params[(hidden_layer_1_size * (input_layer_size + 1)) + hidden_layer_2_size * (hidden_layer_1_size + 1):],
-    (output_layer_size, (hidden_layer_2_size + 1)))
+count = 0
+Theta = []	# Theta[0], Theta[1], Theta[2] - соответствующие матрицы весов 
+for i in range(len(layers)-1):
+	Theta.append(np.reshape(nn_params[count: count + layers[i+1] * (layers[i] + 1)], (layers[i+1], (layers[i] + 1))))
+	count += layers[i+1] * (layers[i] + 1)
 
 while 1:
-    path = input('Введите путь к изображению (20*20): ')
-    print('Изображение: ' + dictionary[functions.predict(functions.generate_vector(path), weights_h_1, weights_h_2, weights_o)])
+    path = input('Введите путь к изображению: ')
+    print('Изображение: ' + dictionary[functions.predict(functions.generate_vector(path), Theta)])
